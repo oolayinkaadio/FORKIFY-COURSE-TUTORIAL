7679a7090a37d777698299d00fb280dc6a1215ce
Olaosebikan <oolayinkaadio@gmail.com>
2020-12-26 17:27:00 +0100

    modified the src/js/sampleCongig.js file

--- src/js/sampleConfig.js
+++ src/js/sampleConfig.js
@@ -3,3 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = `GENERATE API KEY FROM 'https://forkify-api.herokuapp.com/v2'`;
 export const MODAL_CLOSE_SEC = 2.5;
+const completedProjectUrl = 'https://forkify-olayinka.netlify.app/';
